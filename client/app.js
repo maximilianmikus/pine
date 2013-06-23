e4aeca188e59f92dd1baeee023160b37a8882f1c
Maximilian Mikus <maximilianmikus@gmail.com>
2013-06-23 21:43:13 +0200

    wip

--- client/app.js
+++ client/app.js
@@ -9,7 +9,9 @@ Template.hello.greeting = function () {
 
 Template.hello.events({
   'click input' : function () {
-    
+    Meteor.apply('nodeioScrape', ['http://www.immobilien.net/Eigentumswohnungen/Wien/Seite1/treffer.aspx?iskauf=1&kaufpreisbis=100000', '.results > a'], function (err, res) {
+      console.log(res);
+    });
   }
 });
 
--- packages/node.io/nodeio.js
+++ packages/node.io/nodeio.js
@@ -1,12 +1,16 @@
-var nodeio = Npm.require("node.io");
+nodeio = Npm.require("node.io");
 var Future = Npm.require("fibers/future");
-NODEIO = nodeio;
 
 Meteor.methods({
   nodeioScrape: function (url, selector) {
-    var future = new Future;
-    var scrape = NODEIO.scrape(
-      function() {
+    console.log(nodeio);
+    console.log(url);
+    console.log(selector);
+
+    var future = new Future();
+    var job = new nodeio.Job({
+      input: false,
+      run: function() {
         this.getHtml(url, function(err, $) {
           var res = [];
           $(selector).each(function(title) {
@@ -14,7 +18,23 @@ Meteor.methods({
           });
           future.return(res);
         });
+      }
+    });
+    nodeio.start(job, {}, function (err, output) {
+      console.log("output", output);
+      future.return(output);
+    }, true);
+    /*
+    var scrape = NODEIO.scrape(function() {
+      this.getHtml(url, function(err, $) {
+        var res = [];
+        $(selector).each(function(title) {
+            res.push(title.innerHTML);
+        });
+        future.return(res);
       });
+    }, false);
+    */
     return future.wait();
   }
 });
